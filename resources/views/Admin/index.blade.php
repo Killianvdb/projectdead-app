2ca3a0d4138176ac4af7be2e4a5cd1270081c0da
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-13 00:44:18 +0100

    core: update basic files

--- resources/views/Admin/index.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.')
-@section('content')
-    <h1>Admin dashboard</h1>
-@endsection
--- resources/views/Users/index.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.')
-@section('content')
-    <h1>Users dashboard</h1>
-@endsection
