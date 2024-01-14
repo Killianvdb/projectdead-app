372692d7e0d0c6dbd284720574451e4cdaced051
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-14 20:04:20 +0100

    fix: usercontroller on admin control as user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,49 +6,70 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 
+
 class UserController extends Controller
 {
     public function show($id)
-    {
-        $user = User::find($id);
+{
+    $user = User::find($id);
 
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-
-        if ($user == auth()->user()) {
-            return redirect('/profile');
-        }
-        else {
-            return view('profile.default', ['user' => $user]);
-        }
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
     }
+
+    if ($user == auth()->user()) {
+        return redirect('/profile');
+    } else {
+        return view('profile.default', ['user' => $user]); // Pass $user to the view
+    }
+}
+
 
     public function promoteToAdmin($id)
     {
+        // controleren als de huidige gebruiker een admin is
+        if (auth()->user()->is_admin) {
+            return redirect()->back()->with('error', 'You do not have the necessary permissions.');
+        }
+
         $user = User::find($id);
 
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
 
-        $user->is_admin = true;
-        $user->save();
+        // geen user promoveren die al admin is
+        if (!$user->is_admin) {
+            $user->is_admin = true;
+            $user->save();
 
-        return redirect()->back()->with('success', 'User promoted to admin successfully!');
+            return redirect()->back()->with('success', 'User promoted to admin successfully!');
+        }
+
+        return redirect()->back()->with('error', 'User is already an admin.');
     }
 
     public function demoteFromAdmin($id)
     {
+        //kijken of de huidige gebruiker een admin is
+        if (auth()->user()->is_admin ) {
+            return redirect()->back()->with('error', 'You do not have the necessary permissions.');
+        }
+
         $user = User::find($id);
 
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
 
-        $user->is_admin = false;
-        $user->save();
+        // maak zeker dat je geen admin demote die geen admin is
+        if ($user->is_admin) {
+            $user->is_admin = false;
+            $user->save();
 
-        return redirect()->back()->with('success', 'User demoted from admin successfully!');
+            return redirect()->back()->with('success', 'User demoted from admin successfully!');
+        }
+
+        return redirect()->back()->with('error', 'User is not an admin.');
     }
 }
