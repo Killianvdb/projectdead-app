0a94bf345c2f26c1552f8e8256ef307d5623c9b4
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-14 21:57:36 +0100

    feat: adminuser seeder created

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminExists = User::where('email', 'admin@ehb.be')->exists();
+
+        if (!$adminExists) {
+            User::create([
+                'name' => 'admin',
+                'email' => 'admin@ehb.be',
+                'password' => Hash::make('Password!321'),
+                'is_admin' => true,
+            ]);
+        }
+    }
+}
