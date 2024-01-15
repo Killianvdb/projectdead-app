87b0fb58e56495e05c95d3de802c64853e11d5ca
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-15 18:31:01 +0100

    feat: dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Post;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        //retrieving posts by publishing date so i can show them on the blade in chronological order
+        $posts = Post::OrderBy('publishing_date', 'desc')->get();
+
+        return view('dashboard', compact('posts'));
+    }
+
+    public function search(Request $request)
+    {
+        // we get the searched username from the request
+        $search = $request->input('search');
+
+        // returning the same blade that expects posts
+        $posts = Post::orderBy('publishing_date', 'desc')->get();
+
+        //we use it in the query
+        $users = User::where('username', 'like', '%' . $search . '%')->get();
+
+        return view('dashboard', compact('users', 'search', 'posts'));
+    }
+
+    //user from search result is passed to this controller 
+    public function showProfile(User $user): View
+    {
+        //the user info is passed via this function to the profile page
+        return view('profile.profile-page', compact('user'));
+
+
+    }
+}
