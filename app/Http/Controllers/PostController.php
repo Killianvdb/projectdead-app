f31b1bf850c4e01d3368b3c9b8c0357acd345470
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-15 18:27:48 +0100

    feat: post features

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function showPostForm()
+    {
+
+        return view('posts.create-posts');
+
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+
+            'title' => ['required', 'string'],
+            'content' => ['required', 'string'],
+            'cover_image' => ['image', 'mimes:jpeg,png']
+
+        ]);
+
+
+        if ($request->hasFile('cover_image')) {
+
+            //same logic as profileController update
+
+            // Get the original file name
+            $originalFileName = $request->file('cover_image')->getClientOriginalName();
+
+            // Generate a unique file name
+            $tempFileName = pathinfo($originalFileName, PATHINFO_FILENAME) . '_' . time() . '.' . $request->file('cover_image')->getClientOriginalExtension();
+
+            //replace spaces with an underscore
+            $coverImageFileName = str_replace(' ', '_', $tempFileName);
+
+            // Store the file on the webserver with a custom name
+            $request->file('cover_image')->storeAs('avatars', $coverImageFileName, 'public');
+
+        }
+
+
+
+        $post = $request->user()->posts()->create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'cover_image' => 'storage/' . 'avatars/' . $coverImageFileName,
+            'publishing_date' => now()
+        ]);
+
+
+
+        $post->save();
+
+        return redirect()->back()->with('success', 'post was saved');
+
+    }
+
+
+    public function editPost(Post $post): View
+    {
+        return view('posts.edit-post', compact('post'));
+    }
+
+
+
+    public function updatePost(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => ['required', 'string'],
+            'content' => ['required', 'string'],
+            'cover_image' => ['image', 'mimes:jpeg,png']
+        ]);
+
+
+        $post->title = $request->title;
+        $post->content = $request->content;
+
+        if ($request->hasFile('cover_image')) {
+
+            // Get the original file name
+            $originalFileName = $request->file('cover_image')->getClientOriginalName();
+
+            // Generate a unique file name
+            $tempFileName = pathinfo($originalFileName, PATHINFO_FILENAME) . '_' . time() . '.' . $request->file('cover_image')->getClientOriginalExtension();
+
+            //replace spaces with an underscore
+            $coverImageFileName = str_replace(' ', '_', $tempFileName);
+
+            // Store the file on the webserver with a custom name
+            $request->file('cover_image')->storeAs('avatars', $coverImageFileName, 'public');
+
+        }
+
+        $post->save();
+
+        return redirect()->back()->with('success', 'Post updated successfully');
+    }
+
+    public function deletePost(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->back()->with('success', 'Post deleted successfully');
+    }
+}
