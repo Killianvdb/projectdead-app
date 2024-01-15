b3de907eacd9fdaccc3fdbd02eb501f0d1867144
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-15 18:29:50 +0100

    feat: add of multiple routes , contact,faq,post,dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\FaqController;
+use App\Http\Controllers\ContactController;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\FaqCategory;
+use App\Models\FaqItem;
+use App\Models\ContactForm;
+
 
 
 /*
@@ -25,6 +35,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//searching and seeing users profile
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::post('/dashboard/search', [DashboardController::class, 'search'])->name('dashboard.search');
+Route::get('/profile/{user}', [DashboardController::class, 'showProfile'])->name('profile.profile');
+
+//faq
+Route::get('/faq', [FaqController::class, 'showFaqPage'])->name('faq.faqPage');
+
+//contact
+Route::get('/contact', [ContactController::class, 'showContact'])->name('contact.contactPage');
+Route::post('/contact/send', [ContactController::class, 'storeContactForm'])->name('contact.store');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -41,8 +63,60 @@ Route::get('profile/admin', [AdminController::class, 'index'])->name('profile.ad
     
 });
 
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    //create post
+    Route::get('/posts/create', [PostController::class, 'showPostForm'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    //edit post
+    Route::get('/posts/{post}/edit', [PostController::class, 'editPost'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'updatePost'])->name('posts.update');
+    //delete post
+    Route::delete('post/{post}', [PostController::class, 'deletePost'])->name('posts.delete');
+    //promote user to admin
+    Route::post('/users/{user}/promote', [UserController::class, 'promoteUser'])->name('users.promote');
+    Route::post('/users/{user}/demote', [UserController::class, 'demoteUser'])->name('users.demote');
+
+
+
+
+    //category form
+    Route::get('/faq/create', [FaqController::class, 'createCategory'])->name('faq.create-category');
+    //store category
+    Route::post('/faq/create/store', [FaqController::class, 'storeCategory'])->name('faq.store-category');
+    //item form
+    Route::get('/faq/{category}/create-item', [FaqController::class, 'createItem'])->name('faq.create-item');
+    //item store
+    Route::post('/faq/{category}/create-item', [FaqController::class, 'storeItem'])->name('faq.store-item');
+    //delete category
+    Route::delete('/faq/category/{category}', [FaqController::class, 'deleteCategory'])->name('faq.delete-category');
+    // Delete Item
+    Route::delete('/faq/item/{item}', [FaqController::class, 'deleteItem'])->name('faq.delete-item');
+    //admin contact page
+    Route::get('/contactAdmin', [ContactController::class, 'showAdminContactPage'])->name('contact.contactAdminPage');
+
+
+});
+Route::get('/posts', [PostController::class, 'showPosts'])->name('posts.show');
+Route::get('/posts/{post}', [PostController::class, 'showPost'])->name('posts.showPost');
+
+Route::get('/faq', [FaqController::class, 'showFaqPage'])->name('faq.faqPage');
+Route::get('/faq/{category}', [FaqController::class, 'showCategory'])->name('faq.showCategory');
+Route::get('/faq/{category}/{item}', [FaqController::class, 'showItem'])->name('faq.showItem');
+
 Route::get('/about', function () {
     return view('Profile/about');
 })->name('about');
+
+Route::get('/faqs', function () {
+    return view('profile/faqPage');
+})->name('faqPage');
+
+Route::get('/welcome/contact', function () {
+    return view('contact/contact-page');
+})->name('contact.page');
+
 
 require __DIR__.'/auth.php';
